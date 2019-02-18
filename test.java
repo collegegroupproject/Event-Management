487051899c33f005a07bce27235774d1627d9144
Aditya-Kumar-Yadav <advyadav16@gmail.com>
2019-02-18 14:55:44 +0530

    Added The Minute To Hour Convertor

--- test.java
+++ test.java
@@ -45,7 +45,11 @@ class events
 
 class test
 {
-	static void EventWithMaximumMultiParticipation(events[] max)
+
+	//Starting Time
+	float startTime = 09.00f;		
+
+	static void eventWithMaximumMultiParticipation(events[] max)
 	{
 		int maxParticipants = 0;
 		String eventName = " ";
@@ -60,6 +64,17 @@ class test
 		System.out.println("Event With Maximum Participation - " + eventName);
 	}
 	
+	static float minutesToHourConversion(int minutes)
+	{	
+		float time;
+		float hrs = minutes / 60;		
+		float mins = minutes % 60;
+		
+		time = hrs + (float)(mins * .01);
+		
+		return time;
+	}
+
 	public static void main(String args[])
 	{
 		boolean isParellelisiumPossible;
@@ -70,14 +85,18 @@ class test
 		participants[1] = new student(102, "Adarsh", 1);
 		participants[2] = new student(103, "Ankit", 3);
 		participants[3] = new student(104, "Akansh", 2);
-		participants[4] = new student(105, "Akshay", 90);
+		participants[4] = new student(105, "Akshay", 2);
 
 		events[] eventsHeld = new events[3];	
 		eventsHeld[0] = new events(001, "Coding", 3, 60, 'F');
 		eventsHeld[1] = new events(002, "Presentation", 3, 5, 'V');
 		eventsHeld[2] = new events(003, "Speech", 5, 10, 'V');
 	
+		//Global Counter - Total Time Alloted
+		//float counter = 8*60; // In Minutes		
 		
+		//Event Clock
+		float eventClock = 0;
 		// MAIN ALGORITHM
 		
 			// All Events Are Fixed		
@@ -97,7 +116,7 @@ class test
 		{	
 			if(participants[i].numberOfEventParticipation > 3)
 			{
-				System.out.println("Number Of Event Participation is More Than The Events of Participant  - " + participants[i].studentID + "Student Name - " + participants[i].studentName);
+				System.out.println("Number Of Event Participation is More Than The Listed Events of Participant  - " + participants[i].studentID + " Student Name - " + participants[i].studentName);
 				isParellelisiumPossible = false;
 				System.exit(0);
 			}
@@ -118,19 +137,20 @@ class test
 		System.out.println("\nParallelisium is " + isParellelisiumPossible);
 
 		
-		EventWithMaximumMultiParticipation(eventsHeld);
+		eventWithMaximumMultiParticipation(eventsHeld);
+		
+
+		//System.out.println("\nTime Check - " + minutesToHourConversion(20));
+		
 		
 
 
 
 
-
-
-
-		for(i = 0; i < 5; i++)
+		/*for(i = 0; i < 5; i++)
 		{
 			System.out.println("\nStudentId - " + participants[i].studentID + "\nStudentName - " + participants[i].studentName + "\nNumberOfEventParticipations - " + participants[i].numberOfEventParticipation + "\n \n");
-		}
+		}*/
 
 
 
